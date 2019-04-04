3a1d83d85e3f54209970d345ce3976124e23191a
Webkidt <niratpam@gmail.com>
2019-04-04 17:39:34 +0100

    Update Forecast Information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = ' https://api.darksky.net/forecast/8ed1d5372530ee028273be2ba6d31b50/' + latitude + ',' + longitude
+    const url = ' https://api.darksky.net/forecast/8ed1d5372530ee028273be2ba6d31b50/' + latitude + ',' + longitude + '?units=si'
 
     request({url, json: true}, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The High today is ' + body.daily.data[0].temperatureHigh + ' and the Low today is ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
